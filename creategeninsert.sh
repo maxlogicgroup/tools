542bef894feebdf8d065f8e197a770eb877061ec
JoshConneely <109955430+JoshConneely@users.noreply.github.com>
2023-02-09 18:11:56 +0000

    Update creategeninsert.sh

--- creategeninsert.sh
+++ creategeninsert.sh
@@ -12,21 +12,6 @@ arg2=""
 arg3=""
 arg4=""
 arg5=""
-arg6=""
-arg7=""
-arg8=""
-arg9=""
-arga=""
-argb=""
-argc=""
-argd=""
-arge=""
-argf=""
-argg=""
-argh=""
-argi=""
-argj=""
-argk=""
 
 # Test up to 20 arguments and store them in $arg1 to $argk
 if [ "$*" != "" ]
@@ -59,101 +44,11 @@ then
   shift
 fi
 
-if [ "$*" != "" ]
-then
-  arg6=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  arg7=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  arg8=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  arg9=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  arga=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  argb=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  argc=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  argd=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  arge=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  argf=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  argg=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  argh=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  argi=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  argj=$1
-  shift
-fi
-
-if [ "$*" != "" ]
-then
-  argk=$1
-  shift
-fi
-
 cd ..
 
 pathToScript=`dirname %0`
 . $pathToScript/commonenv.sh
 
-../java/jre/bin/java -Xmx5G -classpath .:./classes:$MAXIMO_CLASSPATH psdi.tools.GenInsertDbc $arg1 $arg2 $arg3 $arg4 $arg5 $arg6 $arg7 $arg8 $arg9 $arga $argb $argc $argd $arge $argf $argg $argh $argi $argj $argk
+../java/jre/bin/java -Xmx5G -classpath .:./classes:$MAXIMO_CLASSPATH psdi.tools.GenInsertDbc $arg1 $arg2 $arg3 $arg4 $arg5
 
 exit $?' > /opt/IBM/SMP/maximo/tools/maximo/internal/geninsertdbc.sh
